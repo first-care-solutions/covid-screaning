2af00e5ecacf074cfa9cf8d81771ddf07395b1b8
tmanmoseri <tmanmoseri@gmail.com>
2022-04-12 12:30:57 +0200

    Added default routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 
+
 const routes: Routes = [
   { 
       path: '', 
       redirectTo: 'pages/LandingPage',
       pathMatch: 'full'
    },
+   { 
+    path: 'home', 
+    redirectTo: 'pages/LandingPage',
+    pathMatch: 'full'
+ },
+ { 
+  path: 'pages/landing', 
+  redirectTo: 'pages/LandingPage',
+  pathMatch: 'full'
+},
    {
-        path: "**",
-        redirectTo: ""
+        path: '**',
+        redirectTo: ''
 					},
 ];
 
