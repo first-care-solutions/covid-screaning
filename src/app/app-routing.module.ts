7a9f6b064f8f43d16464e88049763a174dbaa16a
tmanmoseri <tmanmoseri@gmail.com>
2022-04-12 12:12:56 +0200

    Added landingpage as default with wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { 
+      path: '', 
+      redirectTo: 'pages/LandingPage',
+      pathMatch: 'full'
+   },
+   {
+        path: "**",
+        redirectTo: ""
+					},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
