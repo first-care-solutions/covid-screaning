c7cc695f2a901528b02a1a66744a0816fe473772
tmanmoseri <tmanmoseri@gmail.com>
2022-04-20 15:43:16 +0200

    added necessary features as per review

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,12 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
-  ionicForm!: FormGroup;
+  ionicForm: FormGroup;
   defaultDate = '1987-06-30';
   isSubmitted = false;
-  validations_form: FormGroup;
 
-  constructor(public formBuilder: FormBuilder, private router: Router) {}
+  async openToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Please provide all the required values!',
+    });
+    toast.present();
+    toast.onDidDismiss().then((val) => {
+      console.log('Toast Dismissed');
+    });
+  }
+
+  constructor(public formBuilder: FormBuilder, public toastCtrl: ToastController, public modalController: ModalController, private router: Router) {}
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
@@ -22,10 +33,10 @@ export class LandingPageComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       surname: ['', [Validators.required, Validators.minLength(2)]],
       idType: ['', [Validators.required]],
-      idNumber: ['', [Validators.required, Validators.minLength(5)]],
+      idNumber: ['', [Validators.required, Validators.minLength(9)]],
       dateOfBirth: [this.defaultDate],
       email: ['', [, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]],
-      cellNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      cellNumber: ['', [Validators.required, Validators.minLength(9)]],
     });
   }
   getDate(event) {
@@ -44,8 +55,37 @@ export class LandingPageComponent implements OnInit {
       return false;
     } else {
       console.log(this.ionicForm.value);
-      return true;
       this.router.navigate(['/pages/virtual-room']);
+      return true;
     }
+
+    this.ionicForm.reset();
   }
+
+  /*
+  async openModal() {
+
+    // create the modal window
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      componentProps: { ticketholders: listProps },
+    });
+    // handle the return data when the modal window closes
+    modal.onDidDismiss().then((dataReturned) => {
+      console.log(dataReturned);
+      if (dataReturned.data !== 'Cancel') {
+        this.dataReturned = `You selected transactionID ${dataReturned.data}`;
+      }
+      else {
+        this.dataReturned = 'User cancelled the modal page.';
+      }
+    });
+
+  async closeModal() {
+    await this.modalController.dismiss('Cancel');
+  }
+
+  async resetModal() {
+    await this.modalController.dismiss('Reset');
+  }*/
 }
