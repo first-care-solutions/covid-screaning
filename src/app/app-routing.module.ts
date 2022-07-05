0717362af31e1f62f22965dbe70909f7813d7448
WhiteThundra <arno24589@gmail.com>
2022-07-05 15:05:58 +0200

    all tasks done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { VirtualRoomComponent } from './pages/virtual-room/virtual-room.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pages/landing', pathMatch: 'full' },
   { path: 'pages/landing', component: LandingPageComponent },
   { path: 'home', redirectTo: 'pages/landing', pathMatch: 'full' },
+  { path: 'pages/virtual-room', component: VirtualRoomComponent },
+  { path: 'virtual-room', redirectTo: 'pages/virtual-room', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages/landing', pathMatch: 'full' },
 ];
 
