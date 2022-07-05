b58be29752b4801b0f99a794839da98f22821eca
WhiteThundra <arno24589@gmail.com>
2022-07-05 13:11:13 +0200

    landing page and routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'pages/landing', component: LandingPageComponent },
+  { path: 'home', component: LandingPageComponent },
+  { path: '**', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
