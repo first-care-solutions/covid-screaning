853564128530be881842a1536ba53eb8175a8408
WhiteThundra <arno24589@gmail.com>
2022-07-07 12:09:47 +0200

    All HTML done

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  userInfoForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    initials: ['', Validators.required],
+    firstName: ['', Validators.required],
+    surname: ['', Validators.required],
+    idType: ['', Validators.required],
+    idNumber: ['', Validators.required],
+    dateOfBirth: ['', Validators.required],
+    cellNumber: ['', Validators.required],
+    email: ['', Validators.email],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log();
+  }
 
+  // {
+  //   title: 'MR' | 'MRS' | 'MS' | 'MISS' | 'MASTER' | 'DR' | 'PROF' | 'REV' | 'DS' | null;
+  //   initials: string;
+  //   firstName: string;
+  //   surname: string;
+  //   idType: 0 | 1 | 2; //--- 0=RSA ID 1=Passport 2=DOB/Other
+  //   idNumber: string;
+  //   dateOfBirth: string; // Format must be 'YYYY-MM-DD'
+  //   cellNumber: string;
+  //   email?: string;
+  // }
 }
